b543f3c29a6394bf2296b8174f94e1d30b10318b
Adam Casey <adamncasey@gmail.com>
2022-12-04 23:17:44 +0000

    Day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,36 @@
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut sorted = input.split("\n\n").map(|e| e.split("\n").map(|num| num.parse::<u32>().unwrap()).sum::<u32>()).collect::<Vec<u32>>();
+    sorted.sort();
+    let max = sorted.iter().rev().take(3).sum::<u32>();
+
+    Some(max) 
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    input.split("\n\n").map(|e| e.split("\n").map(|num| {
+        num.parse::<u32>().unwrap()
+    }).sum::<u32>()).max()
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
