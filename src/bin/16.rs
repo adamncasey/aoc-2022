b2cf7ee5d342467785a3159c6995cffc921954d8
Adam Casey <adamncasey@gmail.com>
2022-12-17 19:03:07 -0700

    Day 16 finishes

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap, HashSet, VecDeque};
 #[derive(Debug)]
 struct Node {
     flow_rate: u32,
-    connected: Vec<String>,
+    connected: Vec<(String, u32)>,
 }
 
 fn parse(input: &str) -> HashMap<String, Node> {
@@ -22,7 +22,10 @@ fn parse(input: &str) -> HashMap<String, Node> {
             .split_once("; tunnels lead to valves ")
             .unwrap_or_else(|| rest.split_once("; tunnel leads to valve ").unwrap());
 
-        let connected = rest.split(", ").map(String::from).collect::<Vec<String>>();
+        let connected = rest
+            .split(", ")
+            .map(|n| (String::from(n), 1))
+            .collect::<Vec<(String, u32)>>();
 
         output.insert(
             String::from(node),
@@ -72,19 +75,32 @@ fn cave_dfs(
 
     let cave = caves.get(current).unwrap();
 
+    let mut moved = false;
+
     for neighbour in cave.connected.iter() {
         //path.push(format!("move to {neighbour}"));
+        if time_remaining < neighbour.1 {
+            // impossible move
+            continue;
+        }
+
+        if open.contains(&neighbour.0) {
+            // pointless move
+            continue;
+        }
+
         cave_dfs(
             caves,
-            neighbour,
+            &neighbour.0,
             open.clone(),
-            score,
+            score + (neighbour.1 - 1) * flow_rate,
             flow_rate,
-            time_remaining - 1,
+            time_remaining - neighbour.1,
             best_so_far,
             max_score_estimates,
             path,
         );
+        moved = true;
         //path.pop();
     }
 
@@ -105,32 +121,25 @@ fn cave_dfs(
             path,
         );
 
+        moved = true;
+
         //path.pop();
     }
-}
 
-pub fn part_one(input: &str) -> Option<u32> {
-    // more than 1315
-
-    let caves = parse(input);
-
-    println!("{caves:?}");
-
-    let mut best_so_far = 1650;
-
-    cave_dfs(
-        &caves,
-        "AA",
-        HashSet::new(),
-        0,
-        0,
-        30,
-        &mut best_so_far,
-        &calc_max_scores(&caves),
-        &mut Vec::new(),
-    );
-
-    Some(best_so_far)
+    if !moved {
+        // ran out of moves, let's jump to the end?
+        cave_dfs(
+            caves,
+            current,
+            open,
+            score,
+            flow_rate,
+            time_remaining - 1,
+            best_so_far,
+            max_score_estimates,
+            path,
+        );
+    }
 }
 
 fn calc_max_scores(caves: &HashMap<String, Node>) -> HashMap<u32, u32> {
@@ -157,6 +166,95 @@ fn calc_max_scores(caves: &HashMap<String, Node>) -> HashMap<u32, u32> {
     }
 
     output
+}
+
+fn calc_dist(
+    caves: &HashMap<String, Node>,
+    start: &str,
+    dest: &str,
+    visited: &mut HashSet<String>,
+) -> u32 {
+    if start == dest {
+        return 0;
+    }
+
+    let mut best = 100000;
+    for (n, _) in caves.get(start).unwrap().connected.iter() {
+        if visited.contains(n) {
+            continue;
+        }
+
+        visited.insert(n.clone());
+        let dist = 1 + calc_dist(caves, n, dest, visited);
+        visited.remove(n);
+        best = std::cmp::min(best, dist);
+    }
+
+    best
+}
+
+fn simplify_caves(caves: HashMap<String, Node>) -> HashMap<String, Node> {
+    let mut nodes_with_flow = caves
+        .iter()
+        .filter(|(name, node)| node.flow_rate > 0)
+        .map(|(name, _)| name)
+        .cloned()
+        .collect::<Vec<String>>();
+
+    nodes_with_flow.push(String::from("AA"));
+
+    let mut output = HashMap::new();
+
+    for n in nodes_with_flow.iter() {
+        let mut connected = Vec::new();
+        for dest in nodes_with_flow.iter() {
+            if n == dest {
+                continue;
+            }
+            connected.push((
+                String::from(dest),
+                calc_dist(&caves, n, dest, &mut HashSet::new()),
+            ));
+        }
+
+        output.insert(
+            n.clone(),
+            Node {
+                flow_rate: caves.get(n).unwrap().flow_rate,
+                connected,
+            },
+        );
+    }
+
+    output
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    // more than 1315
+
+    let caves = parse(input);
+
+    println!("{caves:?}");
+
+    let caves = simplify_caves(caves);
+
+    println!("{caves:?}");
+
+    let mut best_so_far = 1650;
+
+    cave_dfs(
+        &caves,
+        "AA",
+        HashSet::new(),
+        0,
+        0,
+        30,
+        &mut best_so_far,
+        &calc_max_scores(&caves),
+        &mut Vec::new(),
+    );
+
+    Some(best_so_far)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
